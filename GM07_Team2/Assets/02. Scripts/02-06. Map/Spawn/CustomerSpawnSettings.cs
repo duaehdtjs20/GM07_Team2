e805c591284f56596ad4f6063b3a5f341020cb85
sooji <p21266044@gmail.com>
2026-08-04 19:48:20 +0900

    feat | 시간별 램덤스폰인터벌

---
+++ GM07_Team2/Assets/02. Scripts/02-06. Map/Spawn/CustomerSpawnSettings.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CustomerSpawnSettings", menuName = "ScriptableObjects/CustomerSpawnSettings")]
+public class CustomerSpawnSettings : ScriptableObject
+{
+    [SerializeField]
+    private List<CustomerSpawnPeriod> spawnPeriodList;
+    [SerializeField]
+    private AnimationCurve _spawnRateCurve;
+
+    public bool TryGetSpawnPeriod(float hour, out CustomerSpawnPeriod spawnPeriod)
+    {
+        foreach(CustomerSpawnPeriod period in spawnPeriodList)
+        {
+            if(!period.IsInPeriod(hour))
+            {
+                continue;
+            }
+            spawnPeriod = period;
+            return true;
+        }
+
+        spawnPeriod = null;
+        return false;
+    }
+
+    public float GetStoreLevelSpawnRate(int level)
+    {
+        float spawnRate = _spawnRateCurve.Evaluate(level);
+
+        return Mathf.Max(1f, spawnRate);
+    }
+}
