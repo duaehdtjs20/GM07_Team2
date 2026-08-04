8f68aba59cf72188b65626feb52061535a730466
YDS <82944267+duaehdtjs20@users.noreply.github.com>
2026-08-04 14:24:05 +0900

    feat | 손님 FSM 틀 구현

---
+++ GM07_Team2/Assets/02. Scripts/02-08. Customer/CustomerStateBase.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public abstract class CustomerStateBase : ICharacterState
+{
+    protected Customer _customer;
+    public CustomerStateBase(Customer customer)
+    {
+        _customer = customer;
+    }
+    public virtual void Enter()
+    {
+    }
+
+    public virtual void Exit()
+    {
+    }
+
+    public virtual void Update()
+    {
+    }
+}
