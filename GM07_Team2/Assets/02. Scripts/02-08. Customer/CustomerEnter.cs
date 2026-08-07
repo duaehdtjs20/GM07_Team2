842898796a9506316b09985c5b2daaf20ec0a716
YDS <82944267+duaehdtjs20@users.noreply.github.com>
2026-08-07 16:21:05 +0900

    feat | 손님 에셋 적용

--- GM07_Team2/Assets/02. Scripts/02-08. Customer/CustomerEnter.cs
+++ GM07_Team2/Assets/02. Scripts/02-08. Customer/CustomerEnter.cs
@@ -3,26 +3,23 @@
 // 스폰 직후 빈자리 찾아서 이동하는 상태
 public class CustomerEnter : CustomerStateBase
 {
-    public CustomerEnter(Customer customer) : base(customer) { }
+    public CustomerEnter(Customer customer, Animator animator) : base(customer, animator, Animator.StringToHash("Walk")) { }
     public override void Enter()
     {
+        base.Enter();
+
         // 빈 자리를 목표로 지정
         _customer.SetDestination(_customer.Seat.Anchor);
-
-        // 상태 변경 임시 확인
-        _customer.SetColor(Color.red);
     }
     public override void Update()
     {
         // 일정 거리 이상 일 때는 무시
-        if(_customer.CalculateSqrMagnitude() > 1f)
+        if(_customer.CalculateSqrMagnitude() > 2f)
         {
             return;
         }
         // 상태 변경
+        _customer.StopAgent();
         _customer.StateMachine.TransitionTo(_customer.StateMachine.SeatState);
-    }
-    public override void Exit()
-    {
     }
 }
