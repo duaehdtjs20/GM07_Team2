d4ce5b2c1b560cb7335d62aa2eb92c44bca96db8
sooji <p21266044@gmail.com>
2026-08-28 16:22:06 +0900

    feat | 와사비모자이크기능 추가

--- GM07_Team2/Assets/02. Scripts/02-02. UI/UI_TitleScene.cs
+++ GM07_Team2/Assets/02. Scripts/02-02. UI/UI_TitleScene.cs
@@ -1,23 +1,30 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_TitleScene : MonoBehaviour
 {
+    [Header("Buttons")]
     [SerializeField]
     private Button _newGameButton;
     [SerializeField]
     private Button _continueButton;
     [SerializeField]
     private Button _exitButton;
+    [Header("Loading")]
+    [SerializeField]
+    private GameObject _loadingPanel;
 
     private EffectBase _titleEffect;
+    private bool _isLoading;
     private void Awake()
     {
         _newGameButton?.onClick.AddListener(OnClickNewGame);
         _continueButton?.onClick.AddListener(OnClickContinue);
         _exitButton?.onClick.AddListener(OnClickExit);
         _titleEffect = GetComponent<EffectBase>();
+        _loadingPanel?.SetActive(false);
         RefreshButton();
     }
     private void Start()
@@ -41,13 +48,39 @@ public class UI_TitleScene : MonoBehaviour
 
     private void OnClickNewGame()
     {
-        GameSession.StartMode = EGameStartMode.NewGame;
-        SceneManager.LoadScene(ESceneName.Maptest.ToString());
+        StartGame(EGameStartMode.NewGame);
     }
     private void OnClickContinue()
     {
-        GameSession.StartMode = EGameStartMode.Continue;
-        SceneManager.LoadScene(ESceneName.Maptest.ToString());
+        StartGame(EGameStartMode.Continue);
+    }
+
+    private void StartGame(EGameStartMode startMode)
+    {
+        if (_isLoading) 
+        { 
+            return; 
+        }
+        GameSession.StartMode = startMode;
+        StartCoroutine(LoadMainSceneCo());
+    }
+
+    private IEnumerator LoadMainSceneCo()
+    {
+        _isLoading = true;
+        _loadingPanel?.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(ESceneName.Maptest.ToString());
+        while (operation.progress < 0.9f)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            yield return null;
+        }
+        yield return null;
+        operation.allowSceneActivation = true;
+        while(!operation.isDone)
+        {
+            yield return null;
+        }
     }
 
     private void OnClickExit()
