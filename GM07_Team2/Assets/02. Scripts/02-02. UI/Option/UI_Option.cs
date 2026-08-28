54bde3150117db3f938cfb04a34173557cbc7aba
YDS <82944267+duaehdtjs20@users.noreply.github.com>
2026-08-28 16:19:10 +0900

    feat | Option UI 추가

---
+++ GM07_Team2/Assets/02. Scripts/02-02. UI/Option/UI_Option.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class UI_Option : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _optionPanel;
+    [SerializeField]
+    private Button _closeButton;
+
+    private void Awake()
+    {
+        if (_closeButton != null)
+        {
+            _closeButton.onClick.AddListener(Save);
+        }
+    }
+
+    private void Update()
+    {
+        if (!Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            return;
+        }
+        _optionPanel.SetActive(!_optionPanel.activeSelf);
+        if (!_optionPanel.activeSelf)
+        {
+            Save();
+        }
+    }
+    private void Save()
+    {
+        if (SaveManager.Instance != null)
+        {
+            SaveManager.Instance.SaveOption();
+        }
+    }
+}
